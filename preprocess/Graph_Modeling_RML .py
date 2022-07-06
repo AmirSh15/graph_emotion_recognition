9ef768e98904da78131910b8002f706a72156067
Amir <Amir.Shirian@warwick.ac.uk>
2022-07-06 22:55:48 +0100

    Update Graph_Modeling_RML .py

--- preprocess/Graph_Modeling_RML .py
+++ preprocess/Graph_Modeling_RML .py
@@ -49,10 +49,7 @@ Graph_train_data = np.zeros(shape=(1, fix_length, 68, 2))
 train_label = np.zeros(shape=(1, 1))
 
 q = 0
-c = 0  # counter of all data
-c_p = 0  # counter of persons
-a = np.zeros(1)
-for Name in partition['train']:
+for c, Name in enumerate(partition['train']):
 
     cap = cv2.VideoCapture(str(Name))
 
@@ -61,25 +58,43 @@ for Name in partition['train']:
         print("Error opening video stream or file")
 
     train_data = np.zeros(shape=(1, 68, 2))
+    
+    # used to count the number of frames
+    cou = 0
 
     # Read until video is completed
     while (cap.isOpened()):
         # Capture frame-by-frame
         ret, frame = cap.read()
         if ret == True:
-            dir = 'RML_data/' + str(q) + '/'
-            if (not os.path.isdir(dir)):
-                os.mkdir(dir)
-            cv2.imwrite(dir + str(c_p) + '.jpg', frame)
-            c_p += 1
+            # make sure the files has more than 40 frames and start processing afterwards to avoid glitch issues in some videos.
+            if(cou >40):
+                preds = fa.get_landmarks(frame)
+                if (preds != None):
+
+                    if (preds.__len__() > 1):
+                        train_data = np.concatenate((train_data, np.reshape(preds[0], newshape=(1, 68, 2))), axis=0)
+                    else:
+                        train_data = np.concatenate((train_data, preds), axis=0)
 
         # Break the loop
         else:
             break
+        cou += 1
 
     # When everything done, release the video capture object
     cap.release()
     print('Number of files:'+str(c))
+    
+    train_data = train_data[1:]
+
+    # Build nodes' atributes
+    if (train_data.shape[0] > 1):
+        Res = train_data.shape[0] % fix_length
+        for i in range(np.int(train_data.shape[0] / fix_length)):
+            void = np.reshape(train_data[i * fix_length:(i + 1) * fix_length], newshape=(1, fix_length, 68, 2))
+            Graph_train_data = np.concatenate((Graph_train_data, void), axis=0)
+            train_label = np.concatenate((train_label, np.reshape(labels['train'][c], newshape=(1, 1))), axis=0)
 
 
 
